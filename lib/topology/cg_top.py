c771d1da71f4062895cfbde8feead57ed973b31c
Shirui816 <2361394+Shirui816@users.noreply.github.com>
2022-06-01 05:43:58 +0800

    Update

--- lib/topology/cg_top.py
+++ lib/topology/cg_top.py
@@ -6,9 +6,9 @@ def read_cg_topology(cg_system, monomers):
     for bond in cg_system.data['bond']:
         bond_type, i, j = bond[0], bond[1], bond[2]
         type_i, type_j = cg_system.data['type'][i], cg_system.data['type'][j]
-        cg_sys.add_node(i, type=type_i, reaction_type=monomers[type_i]['reaction_type'],
+        cg_sys.add_node(i, type=type_i, reaction_type=monomers[type_i].get('reaction_type'),
                         smiles=monomers[type_i]['smiles'])
-        cg_sys.add_node(j, type=type_j, reaction_type=monomers[type_j]['reaction_type'],
+        cg_sys.add_node(j, type=type_j, reaction_type=monomers[type_j].get('reaction_type'),
                         smiles=monomers[type_j]['smiles'])
         cg_sys.add_edge(i, j, bond_type=bond_type)
     cg_molecules = [cg_sys.subgraph(c).copy() for c in nx.connected_components(cg_sys)]
---
+++ make_paa.py
@@ -0,0 +1,66 @@
+from multiprocessing import Pool
+
+from rdkit import Chem
+from rdkit.Chem import rdChemReactions
+
+from lib.forcefield.opls import ff
+from lib.parse import parse
+from lib.reactor.reactor import Reactor
+from lib.reactor.utils import set_molecule_id_for_h
+from lib.writer.xml_writer import write_xml
+
+__doc__ = """CE maker
+tests/ce: python ../../make_ce.py -X monomerCG.xml -M 'A,c1cc(OC#N)ccc1C(C)(C)c1ccc(OC#N)cc1' -F opls
+(reaction_type: (reaction, probability, needed production ids))
+"""
+
+reaction_templates = {
+    ('A', 'A'): {
+        'reactants_tuple': ('A', 'A'),
+        'reactions':
+            [
+                (rdChemReactions.ReactionFromSmarts(
+                    "[#6:1]#[#6:2].[#6:3]#[#6:4]>>[#6:1]=[#6:2]=[#6:3]=[#6:4]"
+                ), 1.0, None)
+            ],
+    },
+    ('A', 'A', 'A'): {
+        'reactions':
+            [
+                (rdChemReactions.ReactionFromSmarts(
+                    "[C:1]#[C:2].[C:3]#[C:4].[C:5]#[C:6]>>[c:1]1[c:2][c:3][c:4][c:5][c:6]1"
+                ), 1.0, None)
+            ]
+    }
+}
+
+cg_sys, cg_mols, monomers, box, xml = parse()
+reactor = Reactor(monomers, reaction_templates)
+dimers = xml.data.get("di")
+trimers = xml.data.get('tri')
+reactions = []
+# for dimer in dimers:
+# reactions.append((('A', 'A'), dimer[0], dimer[1]))
+for trimer in trimers:
+    reactions.append((('A', 'A', 'A'), trimer[0], trimer[1], trimer[2]))
+aa_sys, meta = reactor.process(cg_sys, reactions)
+aa_mols = list(Chem.rdmolops.GetMolFrags(aa_sys, asMols=True))
+[Chem.SanitizeMol(_) for _ in aa_mols]
+aa_mols_h = [Chem.AddHs(m) for m in aa_mols]
+aa_mols_h = [set_molecule_id_for_h(mh) for mh in aa_mols_h]
+
+defaults = {"O": "@atom:122", "N": "@atom:708"}
+
+
+def processing(i):
+    molecule = aa_mols_h[i]
+    plm_h_ff, bonds, angles, dihedrals = ff(molecule, defaults=defaults)
+    # TODO: Add SMARTS support, for example, defualts={"O": {SMARTS_1: Type_1}...}
+    write_xml(plm_h_ff, box, bonds, angles, dihedrals, '%06d' % i)
+
+
+if __name__ == "__main__":
+    p = Pool(20)
+    p.map(processing, range(len(aa_mols_h)))
+    p.close()
+    p.join()
